4a793fc4a30ce2f1669d7c74d692dd7bc9aa3665
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-05-20 15:53:55 -0700

    Bell Animation

--- BellAnimationForContacts/ContentView.swift
+++ BellAnimationForContacts/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var rotateBell:Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Text("Physics Based Animation")
+                .offset(y: -150)
+                Image(systemName: "bell.fill")
+                .font(.largeTitle)
+            .scaleEffect(2)
+                .foregroundColor(Color.blue)
+                    .rotationEffect(.degrees(rotateBell ? -25 : 60))
+                    .animation(Animation.spring(response: 0.87, dampingFraction: 0.1, blendDuration: 5).repeatForever(autoreverses: false).speed(4))
+                    .onAppear(){
+                        self.rotateBell.toggle()
+            }
+            
+            Circle()
+            .frame(width: 25, height: 25)
+                .foregroundColor(Color.pink)
+                .offset(y: -35)
+            Text("2")
+                .font(.body)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .offset(y: -35)
+            
+            
+            
+            
+            
+            
+            
+        }
     }
 }
 
