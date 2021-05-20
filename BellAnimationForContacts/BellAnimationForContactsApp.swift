ea87f491e2a242798636a1c52cf21480ba91205e
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-05-20 15:37:32 -0700

    Initial Commit

---
+++ BellAnimationForContacts/BellAnimationForContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BellAnimationForContactsApp.swift
+//  BellAnimationForContacts
+//
+//  Created by Vadiraj Hippargi on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct BellAnimationForContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
